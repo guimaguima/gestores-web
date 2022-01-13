ae83bbace5869d1449c96ff33b72881c81c5b0ed
Gabriel Guimarães <67638212+guimaguima@users.noreply.github.com>
2022-01-13 12:13:18 -0300

    Add files via upload

---
+++ SGAB-Gestores/js/script.js
@@ -0,0 +1,73 @@
+
+// Formulário de Cadastro
+function abreFormCadastro() {
+  document.getElementById("formCadastro").style.display = "block";
+}
+
+function fechaFormCadastro() {
+  document.getElementById("formCadastro").style.display = "none";
+}
+
+//Formulário de habilitação
+function abreFormHabilitacao() {
+  document.getElementById("formHabilitacao").style.display = "block";
+}
+
+function fechaFormHabilitacao() {
+  document.getElementById("formHabilitacao").style.display = "none";
+}
+
+//Formulário de Edição
+function abreFormEditar() {
+  document.getElementById("formEdicao").style.display = "block";
+}
+
+function fechaFormEditar() {
+  document.getElementById("formEdicao").style.display = "none";
+}
+
+function abreFormEdicaoConfirmada() {
+  document.getElementById("formEdicao").style.display = "none";
+  document.getElementById("formEdicaoConfirmada").style.display = "block";
+}
+
+function fechaFormEdicaoConfirmada() {
+  document.getElementById("formEdicaoConfirmada").style.display = "none";
+}
+
+//Formulário de Desativação
+function abreFormDesativar() {
+  document.getElementById("formDesativacao").style.display = "block";
+}
+
+function fechaFormDesativar() {
+  document.getElementById("formDesativacao").style.display = "none";
+}
+
+//Formulário de Empréstimo
+function abreFormEmprestar() {
+  document.getElementById("formEmprestimo").style.display = "block";
+}
+
+function fechaFormEmprestar() {
+  document.getElementById("formEmprestimo").style.display = "none";
+}
+
+//Formulário de Devolucao
+function abreFormDevolucao() {
+  document.getElementById("formDevolucao").style.display = "block";
+}
+
+function fechaFormDevolucao() {
+  document.getElementById("formDevolucao").style.display = "none";
+}
+
+//Formulário de Restauração
+function abreFormRestauracao() {
+  document.getElementById("formRestauracao").style.display = "block";
+}
+
+function fechaFormRestauracao() {
+  document.getElementById("formRestauracao").style.display = "none";
+}
+
